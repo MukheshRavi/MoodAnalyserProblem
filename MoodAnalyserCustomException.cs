922c3ea09847bc6d37815fb3f1fca3a44f4d5dfc
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-12 23:05:58 +0530

    [Mukesh].Refactor Test mood analyse Custom Exception method

---
+++ MoodAnalyserCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    class MoodAnalyserCustomException :Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY_MESSAGE,
+            NULL_MESSAGE
+        }
+        public readonly ExceptionType type;
+
+        public MoodAnalyserCustomException(ExceptionType type) : base()
+        {
+            this.type = type;
+        }
+    }
+
+
+}
